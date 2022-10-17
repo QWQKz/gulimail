1509618ab26c0dcee3f1263fec695f857d74b7ef
kxs <1612506470@qq.com>
2022-10-17 18:38:25 +0800

    项目初始结构创建

---
+++ gulimail-menber/src/main/java/com/kzstudy/gulimail/GulimailMenberApplication.java
@@ -0,0 +1,13 @@
+package com.kzstudy.gulimail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimailMenberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimailMenberApplication.class, args);
+    }
+
+}
